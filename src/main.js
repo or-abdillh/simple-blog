1f0b8e87bd415a17bf0785882a4dc1de90ba0609
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-05-28 10:21:30 +0800

    Setup app for tailwind, vue router, styling, and title website

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import router from './router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './style/index.css'
+import './style/app.css'
+
+createApp(App)
+  .use( router )
+  .mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-green-500 text-3xl font-medium">Hello From Home</h1>
+</template>
